16c1078cc593c09127d2b19780747a64c40f7d78
XingWei0920 <eoin19920920@gmail.com>
2023-12-12 01:05:47 +0800

    modified to finish task 1

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -4,14 +4,13 @@ const ArticleCard=(props)=>{
     const {article}=props
     return (
         <li className="itemBlock">
-        <p>Article id: {article.article_id}</p>
         <p>Article Author: {article.author}</p>
         <p>Article Topic: {article.topic}</p>
-        <p>Article Votes: {article.price}</p>
+        <p>Article Votes: {article.votes}</p>
         <p>Article Title: {article.title}</p>
-        <p>Article Created At: {article.created_at}</p>
+        <p>Article Created At: {new Date(article.created_at).toUTCString()}</p>
         <p>Article Comment Count: {article.comment_count}</p>
-        <p>Article Article Img URL: {article.article_img_url}</p>
+        <img src={article.article_img_url} alt="image" width="100" height="100"></img>
         </li>
     )
 }
